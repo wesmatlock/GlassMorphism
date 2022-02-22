803f235f5ee24d1b488321b4cf9fa06ea3d73409
Wesley Matlock <wmatlock@gmail.com>
2022-02-22 09:17:12 -0700

    Initial Commit

---
+++ GlassMorphism/GlassMorphismApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassMorphismApp.swift
+//  GlassMorphism
+//
+//  Created by Wesley Matlock on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct GlassMorphismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
